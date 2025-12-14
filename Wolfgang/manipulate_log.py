d25835ccb2467682a8be55662e4c4f92e80056c9
sWintermoor <sWintermoor@gmail.com>
2025-12-14 22:02:39 +0100

    longer sequence

--- Wolfgang/manipulate_log.py
+++ Wolfgang/manipulate_log.py
@@ -6,7 +6,7 @@ new_log_file = "./Wolfgang/log_shortened.json"
 with open(log_file, "r") as f:
     daten = json.load(f)
     kp = daten["kp"]
-    entries = daten["entries"][:10000]
+    entries = daten["entries"][:50000]
 
     daten = {
         "kp": kp, 
